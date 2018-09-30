3bc0d9e3ec51afcdfd8774bffd8d5fda9c8cccc6
Eric Carlson <eric.paul.carlson@gmail.com>
2018-09-30 09:54:20 -0700

    updated youtube api

--- main.js
+++ main.js
@@ -2,12 +2,12 @@ const APIKEY = 'AIzaSyCjQeMa3AVf38yIkwZAu3icqvSpxwGhmfw';
 
 const YOUTUBE_SEARCH_URL = "https://www.googleapis.com/youtube/v3/search";
 
-function getDataFromApi (searchTerm, callback) {
+function getDataFromApi(searchTerm, callback) {
     const query = {
         part: 'snippet',
         q: searchTerm,
         key: APIKEY,
-        per_page: 5,
+        maxResults: 6,
     }
     $.getJSON(YOUTUBE_SEARCH_URL, query, callback)
 }
@@ -16,17 +16,19 @@ function renderResult(result) {
     return `
     <div>
         <h2>
-        <a class='js-result-name' href='${result.html_url}' target='_blank'>${result.name}</a>
-    </div>
+        <a class='js-result-name' href='https://www.youtube.com/watch?v=${result.id.videoId}' target='_blank'>${result.snippet.title}</a></h2>
+        <a class='js-result-name' href='https://www.youtube.com/channel/${result.snippet.channelId}' target='_blank'>${result.snippet.channelTitle}</a><br>
+        <img src='${result.snippet.thumbnails.medium.url}' height='90' width='120'>
+    </div><br>
     `;
 }
 
-function displayYoutubeSearchData (data) {
+function displayYoutubeSearchData(data) {
     const results = data.items.map((item, index) => renderResult(item));
     $('.js-search-results').html(results);
 }
 
-function watchSubmit (){
+function watchSubmit() {
     $('.js-search-form').submit(event => {
         event.preventDefault();
         const queryTarget = $(event.currentTarget).find('.js-query');
